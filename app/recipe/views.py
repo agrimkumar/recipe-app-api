589128cf3a965b2f353bbfc5b0660a6e363db6bc
Agrim Kumar <agrim4sure@gmail.com>
2021-05-25 02:07:31 +0530

    Added list ingredients feature.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,10 +1,10 @@
-from rest_framework import viewsets, mixins
+from rest_framework import serializers, viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 
-from recipe.serializers import TagSerializer
+from recipe import serializers
 
 
 class TagViewSet(viewsets.GenericViewSet,
@@ -14,7 +14,7 @@ class TagViewSet(viewsets.GenericViewSet,
     authentication_classes = (TokenAuthentication, )
     permission_classes = (IsAuthenticated,)
     queryset = Tag.objects.all()
-    serializer_class = TagSerializer
+    serializer_class = serializers.TagSerializer
 
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
@@ -23,3 +23,15 @@ class TagViewSet(viewsets.GenericViewSet,
     def perform_create(self, serializer):
         """create a new tag"""
         return serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """Manage ingredients in the database"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
+
+    def get_queryset(self):
+        """Returns objects for the current authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
